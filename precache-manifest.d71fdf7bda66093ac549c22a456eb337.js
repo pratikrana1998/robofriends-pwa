07328ff90f76519886a92604618d48b35a220b91
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2021-04-26 00:28:55 +0530

    Updates

---
+++ precache-manifest.d71fdf7bda66093ac549c22a456eb337.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5c36895da5ca4033c24382b867926e",
+    "url": "/robofriends-pwa/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/robofriends-pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6f7d8d9f02e671f0dc7a",
+    "url": "/robofriends-pwa/static/css/main.4fe2c603.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/robofriends-pwa/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends-pwa/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7d8d9f02e671f0dc7a",
+    "url": "/robofriends-pwa/static/js/main.46c73354.chunk.js"
+  },
+  {
+    "revision": "944164b17ae3f023b59b",
+    "url": "/robofriends-pwa/static/js/runtime-main.084995b7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
